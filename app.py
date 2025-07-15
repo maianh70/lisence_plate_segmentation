25b5c025e7b03a1e430b29697f4bccf29eb68b2d
maianh70 <anhdo.mai70@gmail.com>
2025-07-15 15:28:04 +0700

    Done deploy the app. The model still need polished

---
+++ app.py
@@ -0,0 +1,62 @@
+from io import BytesIO
+import streamlit as st
+from ultralytics import YOLO
+import cv2
+import numpy as np
+from PIL import Image
+from helper import mask_extracting, cropping_plate
+
+@st.cache_resource
+def load_model():
+    return YOLO("best.pt")
+
+max_files = 7
+st.title("License Plate Detection")
+st.write("Upload an image and get the plate!!!")
+
+up_loaded_files = st.file_uploader(
+    "Upload images", 
+    type=["jpg", "jpeg", "png"],
+    accept_multiple_files=True
+    )
+
+if len(up_loaded_files) > max_files:
+    st.warning(f"We only process {max_files} at a time")
+
+if not up_loaded_files:
+    st.warning(f"Please upload your images of plates")
+
+
+for i, file in enumerate(up_loaded_files):
+    # Read the uploaded file to images:
+    model = load_model()
+    file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, 1) 
+    # 1 as color (BGR), excluding alpha, 0 for gray scale, 1  loads the image as-is (including alpha if present)
+    im_bgr_to_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    #st.image(im_bgr_to_rgb, caption="Original Image", use_column_width=True)
+
+    with st.spinner("Cutting...."):
+        mask, resized_im, cont = mask_extracting(im_bgr_to_rgb, model)
+        plate = cropping_plate(resized_im, mask, cont)
+
+    
+    st.image(resized_im, caption=f"The image_{i}", use_container_width=True)
+    st.image(plate, caption=f"The plate_{i}", use_container_width=True)
+    
+    img_for_pil = Image.fromarray(plate)
+    buf = BytesIO()
+    img_for_pil.save(buf, format="PNG")
+    result_in_byte = buf.getvalue()
+
+    st.download_button(
+        label="Download Plate",
+        data=result_in_byte,
+        file_name=f"plate{i}.png",
+        mime="image/png",
+        key=f"download_plate_{i}"
+    )
+
+
+
---
+++ helper.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+def mask_extracting(img, model):
+  test = model.predict(source=img, save=True, imgsz=640)
+  read_im = img.copy()
+  mask = test[0].masks.data[0].cpu().numpy()
+  mask = (mask >= 0.3).astype(np.uint8)
+
+  kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+  mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)
+  mask = cv2.dilate(mask, kernel, iterations=1)
+
+  read_im_resized = cv2.resize(read_im, (mask.shape[1], mask.shape[0]))
+  contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+  blank_mask = np.zeros_like(mask, dtype=np.uint8)
+
+  for cnt in contours:
+      cv2.drawContours(blank_mask, [cnt], -1, color=1, thickness=-1)
+
+  return blank_mask * 255, read_im_resized, contours
+
+
+def cropping_plate(img, mask, cont):
+  largest_contour = max(cont, key=cv2.contourArea)
+  x, y, w, h = cv2.boundingRect(largest_contour)
+
+  cropped_image = img[y:y+h, x:x+w]
+  cropped_mask = mask[y:y+h, x:x+w]
+  alpha = np.where(cropped_mask > 0, 255, 0).astype(np.uint8)  
+  plate_bgra = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2RGBA)
+  plate_bgra[:, :, 3] = alpha
+
+  
+  return plate_bgra
